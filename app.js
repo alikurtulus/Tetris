684d07de0c25108d849694b0a871e2d53cc51e53
alikurtulush92 <kopanarya92@gmail.com>
2020-05-19 23:02:00 +0100

    Created board

---
+++ app.js
@@ -0,0 +1,5 @@
+
+ const board = document.querySelector('.grid')
+ // Create your border
+ const myNewBoard = new Board(200,'cell',board)
+ myNewBoard.createBoard(200)
---
+++ board.js
@@ -0,0 +1,17 @@
+class Board {
+    constructor(cellsNumber,className,targetBoard){
+        this.cellsNumber = cellsNumber
+        this.className = className
+        this.targetBoard = targetBoard
+    }
+    createBoard(cellsNumber){
+       for(let i=0; i<cellsNumber;i++){
+          const cell = document.createElement('div')
+          cell.className = this.className
+          this.targetBoard.append(cell)
+          
+       }
+
+
+    }
+}
